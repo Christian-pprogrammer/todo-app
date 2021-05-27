0e508ac003b21726f8126b249ead2328c7577d0a
Christian-pprogrammer <mpanoc6@gmail.com>
2021-05-27 07:42:04 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+  newTodo: any;
+  todos:any = [];
+  trash="../assets/images/si-delete.svg";
+  addTodo(){
+    if(this.newTodo){
+      this.todos.unshift(this.newTodo);
+      this.newTodo = ''
+    }
+  }
+  rub(i: any){
+    this.todos.splice(i,1);
+  }
+  // checked = false;
+  clickMe(event: any){
+    if(event.target.parentNode.childNodes[1].classList.contains("myclass")){
+      event.target.parentNode.childNodes[1].classList.remove("myclass");
+    }else{
+      event.target.parentNode.childNodes[1].classList.add("myclass");
+    }
+  }
 }
